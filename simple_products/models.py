cf72b02173e8948686185dcf15c416e343d9dc21
NL <3h4ws60vh2@unknown.domain>
2013-02-14 00:22:15 +0200

    create fixtures and model

--- simple_products/models.py
+++ simple_products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    descr = models.TextField()
+    width = models.IntegerField()
+    heigth = models.IntegerField()
+    color_1 = models.CharField(max_length=7)
+
+    def __unicode__(self):
+        return self.name
